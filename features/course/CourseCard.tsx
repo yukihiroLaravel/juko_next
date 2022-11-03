72cae53af25c9bc445e78dc42f850607b092d22a
Kou <hikkappi@yahoo.co.jp>
2022-11-03 21:08:43 +0900

    講座カード - スタイル修正。

--- features/course/CourseCard.tsx
+++ features/course/CourseCard.tsx
@@ -1,11 +1,27 @@
 const CourseCard = () => {
   return (
-    <div className="w-[370px] h-auto rounded-t-[10px]">
-      <div className="bg-purple-500 h-[210px] rounded-t-[10px]">{/* <img src="" alt="" /> */}</div>
-      <div className="h-auto  ml-[13px] mt-[16px]">
+    <div className="w-[370px] h-auto border-[#100D59] border-solid border-2 rounded-[10px] overflow-hidden">
+      <div className="bg-purple-500 h-[210px]">{/* <img src="" alt="" /> */}</div>
+      <div className="h-auto  ml-[13px] mt-[16px] relative">
         <p className="font-semibold text-[16px] mb-[16px]">コースタイトル</p>
         <p className="font-semibold text-[16px] mb-[16px]">講師名</p>
         <p className="font-semibold text-[16px] mb-[16px]">進捗 70%</p>
+        <a href="#">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-[24px] h-[24px] absolute right-[17px] bottom-[0px]"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+            />
+          </svg>
+        </a>
       </div>
     </div>
   );
