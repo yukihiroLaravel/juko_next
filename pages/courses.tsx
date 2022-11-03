65bd7973f96c32f155679ce115525d369651b99a
Kou <hikkappi@yahoo.co.jp>
2022-11-03 11:49:36 +0900

    ヘッダーの実装。レスポンシブ未対応。

---
+++ pages/courses.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import Header from '@/components/layouts/Header';
+const Courses: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      <p>テスト</p>
+    </div>
+  );
+};
+
+export default Courses;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './features/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-blue': '#00A5D4',
+      },
+    },
   },
   plugins: [],
 };
