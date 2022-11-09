b28bad39af271f676c9d3ec9e7d5f099ad6db5ed
Kou <hikkappi@yahoo.co.jp>
2022-11-10 06:25:37 +0900

    チャプタータイトルの表示まで実装。

--- pages/course.tsx
+++ pages/course.tsx
@@ -6,28 +6,39 @@ const Courses: NextPage = () => {
   return (
     <>
       <Header />
-      <aside className="w-[300px] h-auto bg-[#F5F5F5]" aria-label="Sidebar">
-        <div className="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
-          <ul className="mt-[30px]">
-            <li className="mb-[20px]">
-              <Image
-                src={process.env.NEXT_PUBLIC_IMAGE_URL + 'course/1/thumbnail.png'}
-                alt="course"
-                height={360}
-                width={640}
-              />
-            </li>
-            <li>
-              <div className="bg-[#FFCDCD] w-full text-center">
-                <p className="font-semibold text-[36px] pt-[30px] pb-[20px]">講座名</p>
-                <p className="font-semibold text-[26px] pt-[20px] pb-[30px]">
-                  100% <span className="font-semibold text-[14px]">完了</span>
-                </p>
-              </div>
-            </li>
-          </ul>
+      <div className="flex">
+        <aside className="shrink-0 w-1/5 h-auto bg-[#F5F5F5]" aria-label="Sidebar">
+          <div className="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
+            <ul className="mt-[30px]">
+              <li className="mb-[20px]">
+                <Image
+                  src={process.env.NEXT_PUBLIC_IMAGE_URL + 'course/1/thumbnail.png'}
+                  alt="course"
+                  height={360}
+                  width={640}
+                />
+              </li>
+              <li>
+                <div className="bg-[#FFCDCD] w-full text-center">
+                  <p className="font-semibold text-[36px] pt-[30px] pb-[20px]">講座名</p>
+                  <p className="font-semibold text-[26px] pt-[20px] pb-[30px]">
+                    100% <span className="font-semibold text-[14px]">完了</span>
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </aside>
+        <div className="w-3/4 mx-auto">
+          <div className="mt-[72px]">
+            <h2 className="font-semibold text-[36px]">コースカリキュラム</h2>
+          </div>
+          <div className="mt-[10px] h-[120px] bg-[#C1E5FF] text-center">
+            <h3 className="font-semibold text-[36px] leading-[120px]">チャプター</h3>
+          </div>
+          <div className="mt-[50px]"></div>
         </div>
-      </aside>
+      </div>
     </>
   );
 };
