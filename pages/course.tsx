5b71aadf890b9bfcd765a9f5bdd1a914c790f87a
Kou <hikkappi@yahoo.co.jp>
2022-11-13 18:13:10 +0900

    レッスン一覧表示まで実装。

--- pages/course.tsx
+++ pages/course.tsx
@@ -39,7 +39,37 @@ const Courses: NextPage = () => {
           <div className="mt-[10px] h-[120px] bg-[#C1E5FF] text-center">
             <h3 className="font-semibold text-[36px] leading-[120px]">チャプター</h3>
           </div>
-          <div className="mt-[50px]"></div>
+          <div className="mt-[50px] mx-auto w-11/12 text-center">
+            <div className="flex flex-start mb-[10px]">
+              <div className="w-[30px] h-[30px] rounded-full bg-[#6D8DFF] mt-[10px]"></div>
+              <div className="bg-[#D9D9D9] w-11/12 ml-5 h-[50px]">
+                <p className="leading-[50px] font-semibold text-[32px]">Lesson1</p>
+              </div>
+            </div>
+            <div className="flex flex-start mb-[10px]">
+              <div className="w-[30px] h-[30px] rounded-full bg-[#D9D9D9] mt-[10px]"></div>
+              <div className="bg-[#D9D9D9] w-11/12 ml-5 h-[50px]">
+                <p className="leading-[50px] font-semibold text-[32px]">Lesson2</p>
+              </div>
+            </div>
+            <div className="flex flex-start mb-[10px]">
+              <div className="w-[30px] h-[30px] rounded-full bg-[#F58909] mt-[10px] text-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="2"
+                  stroke="currentColor"
+                  className="w-5 h-5 mx-auto my-1 text-white"
+                >
+                  <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                </svg>
+              </div>
+              <div className="bg-[#D9D9D9] w-11/12 ml-5 h-[50px]">
+                <p className="leading-[50px] font-semibold text-[32px]">Lesson3</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
