19916be5a76e4fd88c6b87d4685853d8745e984d
Kou <hikkappi@yahoo.co.jp>
2022-11-16 20:42:04 +0900

    アイコンのエラー出力対応。

--- features/lesson/components/StatusIcon.tsx
+++ features/lesson/components/StatusIcon.tsx
@@ -16,11 +16,11 @@ export const StatusIcon: FC<Props> = ({ status }) => {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="2"
+            strokeWidth="2"
             stroke="currentColor"
             className="w-5 h-5 mx-auto my-1 text-white"
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
           </svg>
         </div>
       );
