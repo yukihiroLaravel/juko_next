e29573da6884737fec86df1425e249e929c42c8c
Kou <hikkappi@yahoo.co.jp>
2022-11-19 12:00:31 +0900

    講座一覧 - リンク機能を実装。

--- features/course/components/CourseCardList.tsx
+++ features/course/components/CourseCardList.tsx
@@ -1,6 +1,7 @@
 import { Course } from '@/types/Course';
 import React, { FC } from 'react';
 import CourseCard from './CourseCard';
+import Link from 'next/link';
 
 type Props = {
   courses: Course[];
@@ -11,7 +12,13 @@ const CourseCardList: FC<Props> = (props) => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
       {courses.map((course) => {
-        return <CourseCard key={course.course_id} course={course} />;
+        return (
+          <Link key={course.course_id} href="/course">
+            <a>
+              <CourseCard course={course} />;
+            </a>
+          </Link>
+        );
       })}
     </div>
   );
