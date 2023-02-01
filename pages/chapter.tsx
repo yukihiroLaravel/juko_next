b73f90def1907bcde43c9aebd94275d2cb4fd23f
Kou <hikkappi@yahoo.co.jp>
2023-02-02 07:59:42 +0900

    ローティングを実装。

--- pages/chapter.tsx
+++ pages/chapter.tsx
@@ -3,7 +3,7 @@ import { ToggleButton } from '@/components/elements/ToggleButton';
 import { ProgressBar } from '@/components/elements/ProgressBar';
 import { SideBar } from '@/components/elements/SideBar';
 import { NextPage } from 'next';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Breadcrumb } from '@/components/elements/Breadcrumb';
 import { StatusIcon } from '@/features/lesson/components/StatusIcon';
 import { StatusButton } from '@/features/lesson/components/StatusButton';
@@ -11,6 +11,7 @@ import { Movie } from '@/components/elements/Movie';
 import { useWindowSize } from '@/hooks/useWindowSize';
 import { useFetchChapter } from '@/hooks/useFetchChapter';
 import { useRouter } from 'next/router';
+import { Loading } from '@/components/utils/Loading';
 
 type Query = {
   attendanceId?: string;
@@ -27,6 +28,15 @@ const Chapter: NextPage = () => {
     attendanceId: query.attendanceId,
     chapterId: query.chapterId,
   });
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (chapter !== null) {
+      setIsLoading(false);
+      return;
+    }
+  }, [chapter]);
 
   // パン屑のリンクリスト
   const links = [
@@ -48,130 +58,138 @@ const Chapter: NextPage = () => {
     <>
       <Header />
       <div className="flex">
-        {isShowedSideBar ? (
-          <SideBar>
-            <ul className="mt-[30px]">
-              <li className="mb-[20px]">
-                <div className="">
-                  <p className="text-[18px] font-semibold mb-3">チャプター進捗 33%</p>
-                  <ProgressBar progress={33} />
-                </div>
-              </li>
-              <li>
-                <a href="#">
-                  <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
-                    <p className="text-[18px] text-[#6D8DFF] font-semibold">Lesson 1</p>
-                    <StatusIcon status="in_progress" size="small" />
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
-                    <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 2</p>
-                    <StatusIcon status="not_started" size="small" />
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
-                    <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 3</p>
-                    <StatusIcon status="not_started" size="small" />
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <div className="border-[#F58909] border-t-2 min-h-[65px] flex items-center justify-between">
-                    <p className="text-[18px] text-[#F58909] font-semibold">Lesson 4</p>
-                    <StatusIcon status="completed" size="small" />
-                  </div>
-                </a>
-              </li>
-            </ul>
-            <ToggleButton isShowedSideBar={isShowedSideBar} setIsShowedSideBar={setIsShowedSideBar} />
-          </SideBar>
+        {isLoading ? (
+          <div className="w-3/4 mx-auto min-h-[100vh] mt-10 mb-10">
+            <Loading />
+          </div>
         ) : (
-          <ToggleButton isShowedSideBar={isShowedSideBar} setIsShowedSideBar={setIsShowedSideBar} />
-        )}
-
-        <div className="w-3/4 mx-auto min-h-[100vh] mb-10">
-          <Breadcrumb links={links} />
-          <div className="mt-[20px] border-black border-b pb-5">
-            <h2 className="font-semibold text-[30px] md:text-[36px]">チャプタータイトル</h2>
-          </div>
-          <ul className="md:hidden mt-[30px] border-black border-b">
-            <li className="mb-[20px]">
-              <div className="">
-                <p className="text-[18px] font-semibold mb-3">チャプター進捗 33%</p>
-                <ProgressBar progress={33} />
-              </div>
-            </li>
-            <li>
-              <a href="#">
-                <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
-                  <p className="text-[18px] text-[#6D8DFF] font-semibold">Lesson 1</p>
-                  <StatusIcon status="in_progress" size="small" />
-                </div>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
-                  <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 2</p>
-                  <StatusIcon status="not_started" size="small" />
-                </div>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
-                  <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 3</p>
-                  <StatusIcon status="not_started" size="small" />
-                </div>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="border-[#F58909] border-t-2 min-h-[65px] flex items-center justify-between">
-                  <p className="text-[18px] text-[#F58909] font-semibold">Lesson 4</p>
-                  <StatusIcon status="completed" size="small" />
-                </div>
-              </a>
-            </li>
-          </ul>
-          <div className="mt-5 mx-auto">
-            <h2 className="font-semibold text-[25px] md:text-[30px]">レッスンタイトル</h2>
-          </div>
-          <div className="my-5 overflow-auto">
-            {(width as number) > 0 && (
-              <Movie
-                videoId={'rneIc3LX1vs'}
-                height={(width as number) > 640 ? 405 : 180}
-                width={(width as number) > 640 ? 720 : 320}
-              />
+          <>
+            {isShowedSideBar ? (
+              <SideBar>
+                <ul className="mt-[30px]">
+                  <li className="mb-[20px]">
+                    <div className="">
+                      <p className="text-[18px] font-semibold mb-3">チャプター進捗 33%</p>
+                      <ProgressBar progress={33} />
+                    </div>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
+                        <p className="text-[18px] text-[#6D8DFF] font-semibold">Lesson 1</p>
+                        <StatusIcon status="in_progress" size="small" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
+                        <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 2</p>
+                        <StatusIcon status="not_started" size="small" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
+                        <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 3</p>
+                        <StatusIcon status="not_started" size="small" />
+                      </div>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <div className="border-[#F58909] border-t-2 min-h-[65px] flex items-center justify-between">
+                        <p className="text-[18px] text-[#F58909] font-semibold">Lesson 4</p>
+                        <StatusIcon status="completed" size="small" />
+                      </div>
+                    </a>
+                  </li>
+                </ul>
+                <ToggleButton isShowedSideBar={isShowedSideBar} setIsShowedSideBar={setIsShowedSideBar} />
+              </SideBar>
+            ) : (
+              <ToggleButton isShowedSideBar={isShowedSideBar} setIsShowedSideBar={setIsShowedSideBar} />
             )}
-          </div>
-          <div className="flex justify-start">
-            <StatusButton selected={true}>Lesson未実施</StatusButton>
-            <span className="ml-10" />
-            <StatusButton selected={false}>Lesson開始</StatusButton>
-            <span className="ml-10" />
-            <StatusButton selected={false}>Lesson完了</StatusButton>
-          </div>
-          <div className="mt-5">
-            <p>Index</p>
-            <p>
-              ・本レッスンの概要 0:30~ <br />
-              ・プログラミングとは 1:00~
-              <br />
-              ・PHPとはどんな言語 4:00~ <br />
-              ・PHPで計算してみよう 7:00~ <br />
-              ・まとめ 9:00~
-            </p>
-          </div>
-        </div>
+
+            <div className="w-3/4 mx-auto min-h-[100vh] mb-10">
+              <Breadcrumb links={links} />
+              <div className="mt-[20px] border-black border-b pb-5">
+                <h2 className="font-semibold text-[30px] md:text-[36px]">チャプタータイトル</h2>
+              </div>
+              <ul className="md:hidden mt-[30px] border-black border-b">
+                <li className="mb-[20px]">
+                  <div className="">
+                    <p className="text-[18px] font-semibold mb-3">チャプター進捗 33%</p>
+                    <ProgressBar progress={33} />
+                  </div>
+                </li>
+                <li>
+                  <a href="#">
+                    <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
+                      <p className="text-[18px] text-[#6D8DFF] font-semibold">Lesson 1</p>
+                      <StatusIcon status="in_progress" size="small" />
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
+                      <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 2</p>
+                      <StatusIcon status="not_started" size="small" />
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div className="border-[#B5B5B5] border-t-2 min-h-[65px] flex items-center justify-between">
+                      <p className="text-[18px] text-[#B5B5B5] font-semibold">Lesson 3</p>
+                      <StatusIcon status="not_started" size="small" />
+                    </div>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <div className="border-[#F58909] border-t-2 min-h-[65px] flex items-center justify-between">
+                      <p className="text-[18px] text-[#F58909] font-semibold">Lesson 4</p>
+                      <StatusIcon status="completed" size="small" />
+                    </div>
+                  </a>
+                </li>
+              </ul>
+              <div className="mt-5 mx-auto">
+                <h2 className="font-semibold text-[25px] md:text-[30px]">レッスンタイトル</h2>
+              </div>
+              <div className="my-5 overflow-auto">
+                {(width as number) > 0 && (
+                  <Movie
+                    videoId={'rneIc3LX1vs'}
+                    height={(width as number) > 640 ? 405 : 180}
+                    width={(width as number) > 640 ? 720 : 320}
+                  />
+                )}
+              </div>
+              <div className="flex justify-start">
+                <StatusButton selected={true}>Lesson未実施</StatusButton>
+                <span className="ml-10" />
+                <StatusButton selected={false}>Lesson開始</StatusButton>
+                <span className="ml-10" />
+                <StatusButton selected={false}>Lesson完了</StatusButton>
+              </div>
+              <div className="mt-5">
+                <p>Index</p>
+                <p>
+                  ・本レッスンの概要 0:30~ <br />
+                  ・プログラミングとは 1:00~
+                  <br />
+                  ・PHPとはどんな言語 4:00~ <br />
+                  ・PHPで計算してみよう 7:00~ <br />
+                  ・まとめ 9:00~
+                </p>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
