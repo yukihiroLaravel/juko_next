8da84483fb10321ee241e5afb4e9ce39804b28e9
Kou <hikkappi@yahoo.co.jp>
2023-03-04 20:15:05 +0900

    ログインしていない場合は、ログイン画面へ遷移するように実装。

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { AuthenticatedLayout } from '@/features/login/layout/AuthenticatedLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} className="bg-[#F5F5F5]" />;
+  return (
+    <AuthenticatedLayout>
+      <Component {...pageProps} className="bg-[#F5F5F5]" />
+    </AuthenticatedLayout>
+  );
 }
 
 export default MyApp;
