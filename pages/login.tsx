76b77ac68bbdd230c7e178aeb2f23f9f1da77850
Kou <hikkappi@yahoo.co.jp>
2023-04-09 20:35:57 +0900

    ログイン画面において、認証時のチェックを削除。

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,27 +1,8 @@
 import Header from '@/components/layouts/Header';
 import { LoginForm } from '@/features/login/components/LoginForm';
-import { fetcher } from '@/lib/Fetcher';
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import useSWR from 'swr';
 
 const Login: NextPage = () => {
-  const router = useRouter();
-
-  const { error, isValidating } = useSWR('/api/proxy/api/user', fetcher, {
-    revalidateOnFocus: false,
-    revalidateOnReconnect: false,
-    fallbackData: null,
-    errorRetryCount: 0,
-  });
-
-  useEffect(() => {
-    if (!isValidating && error?.response?.status !== 401) {
-      router.push('/courses');
-    }
-  }, [error, router, isValidating]);
-
   return (
     <>
       <Header isLogin={false} />
