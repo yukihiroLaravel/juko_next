ae5d24e65c6b1586e820725c9ddd369844811a2f
Kou <hikkappi@yahoo.co.jp>
2023-09-01 20:28:58 +0900

    講座一覧取得箇所をSWRを利用して取得するように修正。

--- hooks/useFetchCourses.ts
+++ hooks/useFetchCourses.ts
@@ -1,31 +1,26 @@
-import { Axios } from '@/lib/api';
-import { useEffect, useState } from 'react';
 import { Course } from '@/features/course/types/Course';
 import { Instructor } from '@/types/Instructor';
 import { Attendance } from '@/types/Attendance';
+import { fetcher } from '@/lib/Fetcher';
+import useSWR from 'swr';
 
 type Data = Course & {
   instructor: Instructor;
   attendance: Attendance;
 };
 
-export const useFetchCourses = (args: {
-  setIsLoading: (value: React.SetStateAction<boolean>) => void;
-  setIsError: (value: React.SetStateAction<boolean>) => void;
-}) => {
-  const { setIsLoading, setIsError } = args;
-  const [courses, setCourses] = useState<Data[]>([]);
-  useEffect(() => {
-    Axios.get('/api/v1/course/index?student_id=1')
-      .then((res) => {
-        setCourses(res.data.data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        setIsLoading(false);
-        setIsError(true);
-      });
-  }, [setIsLoading, setIsError]);
-  return [courses] as const;
+export const useFetchCourses = () => {
+  const {
+    data: courses,
+    isLoading,
+    error,
+  } = useSWR<{
+    data: Data[];
+  }>('/api/v1/course/index', fetcher);
+
+  return {
+    courses: courses?.data,
+    isLoading,
+    error,
+  };
 };
--- pages/courses.tsx
+++ pages/courses.tsx
@@ -1,58 +1,52 @@
 import Link from 'next/link';
 import { NextPage } from 'next';
-import { useState } from 'react';
 import { Header } from '@/components/layouts/Header';
-import { Loading } from '@/components/utils/Loading';
 import { useFetchCourses } from '@/hooks/useFetchCourses';
-import { Error } from '@/components/utils/Error';
 import { CourseCard } from '@/features/course/components/CourseCard';
 import { CourseHeader } from '@/features/course/components/CourseHeader';
 import { Thumbnail } from '@/components/elements/Thumbnail';
 import { CourseTitle } from '@/features/course/components/CourseTitle';
 import { AuthWrapper } from '@/features/login/components/AuthWrapper';
+import { Loading } from '@/components/utils/Loading';
+import { Error } from '@/components/utils/Error';
 
 const Courses: NextPage = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [courses] = useFetchCourses({ setIsLoading, setIsError });
+  const { courses, isLoading, error } = useFetchCourses();
 
   return (
     <AuthWrapper>
       <Header />
       <CourseHeader />
+      {isLoading && <Loading />}
+      {error && <Error />}
       <div className="container mx-auto mb-10">
-        {isLoading ? (
-          <Loading />
-        ) : (
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
-            {courses.map((course) => {
-              return (
-                <Link key={course.course_id} href="/course">
-                  <a>
-                    <CourseCard>
-                      <div className="h-auto">
-                        <Thumbnail
-                          src={process.env.NEXT_PUBLIC_IMAGE_URL + course.image}
-                          alt="children"
-                          height={360}
-                          width={640}
-                        />
-                      </div>
-                      <div className="h-auto  ml-[13px] mt-[16px]">
-                        <CourseTitle course={course} />
-                        <p className="font-semibold text-[16px] mb-[16px]">
-                          講師 : {course.instructor.last_name} {course.instructor.first_name}
-                        </p>
-                        <p className="font-semibold text-[16px] mb-[16px]">進捗 {course.attendance.progress}%</p>
-                      </div>
-                    </CourseCard>
-                  </a>
-                </Link>
-              );
-            })}
-          </div>
-        )}
-        {isError && <Error />}
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-[30px]">
+          {courses?.map((course) => {
+            return (
+              <Link key={course.course_id} href="/course">
+                <a>
+                  <CourseCard>
+                    <div className="h-auto">
+                      <Thumbnail
+                        src={process.env.NEXT_PUBLIC_IMAGE_URL + course.image}
+                        alt="course image"
+                        height={360}
+                        width={640}
+                      />
+                    </div>
+                    <div className="h-auto  ml-[13px] mt-[16px]">
+                      <CourseTitle course={course} />
+                      <p className="font-semibold text-[16px] mb-[16px]">
+                        講師 : {course.instructor.last_name} {course.instructor.first_name}
+                      </p>
+                      <p className="font-semibold text-[16px] mb-[16px]">進捗 {course.attendance.progress}%</p>
+                    </div>
+                  </CourseCard>
+                </a>
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </AuthWrapper>
   );
