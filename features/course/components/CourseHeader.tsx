595ef3575e1224bf9a7be62b741ae272eb33444a
Kou <hikkappi@yahoo.co.jp>
2023-09-03 13:15:36 +0900

    講座一覧画面の検索ボックスを実装。

--- features/course/components/CourseHeader.tsx
+++ features/course/components/CourseHeader.tsx
@@ -1,12 +1,16 @@
+import { FC } from 'react';
 import { SearchBox } from './SearchBox';
 
-export const CourseHeader = () => {
+type Props = {
+  updateText: (text: string) => void;
+};
+export const CourseHeader: FC<Props> = ({ updateText }) => {
   return (
     <div className="w-full border-[#100D59] border-b-2 mb-10">
       <div className="mt-10 mb-5 flex justify-between">
         <h2 className="font-semibold text-3xl ml-20">講座一覧</h2>
         <div className="justify-end mr-20">
-          <SearchBox />
+          <SearchBox updateText={updateText} />
         </div>
       </div>
     </div>
--- hooks/useFetchCourses.ts
+++ hooks/useFetchCourses.ts
@@ -3,6 +3,7 @@ import { Instructor } from '@/features/instructor/types/Instructor';
 import { Attendance } from '@/features/attendance/types/Attendance';
 import { fetcher } from '@/lib/Fetcher';
 import useSWR from 'swr';
+import { useState } from 'react';
 
 type Data = Course & {
   instructor: Instructor;
@@ -10,17 +11,21 @@ type Data = Course & {
 };
 
 export const useFetchCourses = () => {
+  const [text, setText] = useState('');
+  const updateText = (text: string) => setText(text);
+
   const {
     data: courses,
     isLoading,
     error,
   } = useSWR<{
     data: Data[];
-  }>('/api/v1/course/index', fetcher);
+  }>(text ? `/api/v1/course/index?text=${text}` : '/api/v1/course/index', fetcher);
 
   return {
     courses: courses?.data,
     isLoading,
     error,
+    updateText,
   };
 };
