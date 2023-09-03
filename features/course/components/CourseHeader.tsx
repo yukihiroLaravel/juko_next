24af25fadf3f7abdf3dbd80de525345106c0f18f
Kou <hikkappi@yahoo.co.jp>
2023-09-03 10:41:55 +0900

    講座一覧画面における検索ボックスの実装。

--- features/course/components/CourseHeader.tsx
+++ features/course/components/CourseHeader.tsx
@@ -1,8 +1,13 @@
+import { SearchBox } from './SearchBox';
+
 export const CourseHeader = () => {
   return (
-    <div className="w-full border-[#100D59] border-b-2 mb-[56px]">
-      <div className="mt-[72px] ml-[56px]">
-        <h2 className="font-semibold text-[36px]">講座一覧</h2>
+    <div className="w-full border-[#100D59] border-b-2 mb-10">
+      <div className="mt-10 mb-5 flex justify-between">
+        <h2 className="font-semibold text-3xl ml-20">講座一覧</h2>
+        <div className="justify-end mr-20">
+          <SearchBox />
+        </div>
       </div>
     </div>
   );
