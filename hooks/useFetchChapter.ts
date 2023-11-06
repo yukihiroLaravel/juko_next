6b70a12621c773acaf6f19539e62080f7450997a
木村誠 <>
2023-11-07 02:33:32 +0900

    レビュー箇所修正

--- hooks/useFetchChapter.ts
+++ hooks/useFetchChapter.ts
@@ -2,37 +2,22 @@ import { Axios } from '@/lib/api';
 import { Chapter } from '@/features/chapter/types/Chapter';
 import { Lesson } from '@/features/lesson/types/Lesson';
 import { LessonAttendance } from '@/features/lessonAttendance/types/LessonAttendance';
+import { Instructor } from '@/features/Instructor/types/Instructor';
+import { Course } from '@/features/Course/types/Course';
 import useSWR from 'swr';
 
 type Data = {
   attendance_id: number;
   progress: number;
-  course: {
-    course_id: number;
-    title: string;
-    image: string;
-    instructor: {
-      instructor_id: number;
-      nick_name: string;
-      last_name: string;
-      first_name: string;
-      email: string;
-    };
-    chapter: {
-      chapter_id: number;
-      title: string;
-      lessons: {
-        lesson_id: number;
-        title: string;
-        url: string;
-        remarks: string;
-        lesson_attendance: {
-          lesson_attendance_id: number;
-          status: string;
-        }[];
-      };
-    };
-  };
+  course: (Course & {
+    instructor: (Instructor & {
+    chapter: (Chapter & {
+      lessons: (Lesson & {
+        lessonAttendance: LessonAttendance;
+        })[];
+      });
+    });
+  });
 };
 
 type Args = {
@@ -42,11 +27,11 @@ type Args = {
 
 export const useFetchChapter = ({ attendanceId, chapterId }: Args) => {
   const fetcher = (url: string) => Axios.get(url).then((res) => res.data);
-  const { data: chapter, mutate } = useSWR<Data | null>(
+  const { data: attendance, mutate } = useSWR<Data | null>(
     // テスト表示のためURL仮指定
     '/api/v1/attendance/1/course/1/chapter/2',
     fetcher
   );
 
-  return [chapter?.data, mutate] as const;
+  return [attendance?.data, mutate] as const;
 };
--- pages/chapter.tsx
+++ pages/chapter.tsx
@@ -47,7 +47,7 @@ const Chapter: NextPage = () => {
   const [width] = useWindowSize();
   const router = useRouter();
   const query: Query = router.query;
-  const [chapter, mutate] = useFetchChapter({
+  const [attendance, mutate] = useFetchChapter({
     attendanceId: query.attendanceId,
     chapterId: query.chapterId,
   });
@@ -65,16 +65,16 @@ const Chapter: NextPage = () => {
 
   const calculateChapterProgeress = (): number => {
     // チャプター取得前は0を返す
-    if (chapter === undefined  || chapter.course.chapter.lessons === undefined) return 0;
+    if (attendance === undefined  || attendance.course.chapter.lessons === undefined) return 0;
 
     // 合計レッスン数
-    const lessonTotalCount = chapter.course.chapter.lessons.length;
+    const lessonTotalCount = attendance.course.chapter.lessons.length;
 
     // 合計レッスン数が0の場合は、0を返す
     if (lessonTotalCount === 0) return 0;
 
     // 進捗完了レッスン数
-    const completedLessonTotalCount = chapter.course.chapter.lessons.filter((lesson) => {
+    const completedLessonTotalCount = attendance.course.chapter.lessons.filter((lesson) => {
       return lesson.lessonAttendance?.status === STATUS_COMPLETED_ATTENDANCE;
     }).length;
 
@@ -82,21 +82,21 @@ const Chapter: NextPage = () => {
   };
 
   useEffect(() => {
-    if (chapter !== undefined) {
+    if (attendance !== undefined) {
       setIsLoading(false);
       if (currentLesson !== null) {
-        const newLesson = chapter.course.chapter.lessons.find((lesson) => lesson.lesson_id === currentLesson.lesson_id);
+        const newLesson = attendance.course.chapter.lessons.find((lesson) => lesson.lesson_id === currentLesson.lesson_id);
         if (newLesson) {
           setCurrentLesson(newLesson);
         }
       } else {
-        const initialLesson = chapter?.course.chapter.lessons[lessonIndex];
+        const initialLesson = attendance?.course.chapter.lessons[lessonIndex];
         if (initialLesson) {
           setCurrentLesson(initialLesson);
         }
       }
     }
-  }, [chapter, currentLesson, lessonIndex]);
+  }, [attendance, currentLesson, lessonIndex]);
 
   // パン屑のリンクリスト
   const links = [
@@ -140,7 +140,7 @@ const Chapter: NextPage = () => {
                         <ProgressBar progress={calculateChapterProgeress()} />
                       </div>
                     </li>
-                    {chapter?.course.chapter.lessons.map((lesson) => {
+                    {attendance?.course.chapter.lessons.map((lesson) => {
                       return (
                         <StyleSideBarList
                           key={lesson.lesson_id}
@@ -162,7 +162,7 @@ const Chapter: NextPage = () => {
               <div className="w-3/4 mx-auto min-h-[100vh] mb-10">
                 <Breadcrumb links={links} />
                 <div className="mt-10 border-black border-b pb-5">
-                  <h2 className="font-semibold text-3xl md:text-4xl">{chapter?.course.title}</h2>
+                  <h2 className="font-semibold text-3xl md:text-4xl">{attendance?.course.title}</h2>
                 </div>
                 <ul className="md:hidden my-5 border-black border-b">
                   <li className="mb-10">
@@ -171,7 +171,7 @@ const Chapter: NextPage = () => {
                       <ProgressBar progress={calculateChapterProgeress()} />
                     </div>
                   </li>
-                  {chapter?.course.chapter.lessons.map((lesson) => {
+                  {attendance?.course.chapter.lessons.map((lesson) => {
                     return (
                       <StyleSideBarList
                         key={lesson.lesson_id}
