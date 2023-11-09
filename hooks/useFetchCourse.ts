1c9dc6a85722b3af5197819e955a7bec55935ce3
Kou <hikkappi@yahoo.co.jp>
2023-11-09 22:38:34 +0900

    Refactor ProgressCard and useFetchCourse hooks

--- hooks/useFetchCourse.ts
+++ hooks/useFetchCourse.ts
@@ -1,4 +1,3 @@
-import { Axios } from '@/lib/api';
 import { Chapter } from '@/features/chapter/types/Chapter';
 import { Course } from '@/features/course/types/Course';
 import { Lesson } from '@/features/lesson/types/Lesson';
@@ -8,14 +7,17 @@ import { Attendance } from '@/features/attendance/types/Attendance';
 import useSWR from 'swr';
 import { fetcher } from '@/lib/Fetcher';
 
-type Data = Course & {
-  instructor: Instructor;
-  attendance: Attendance;
-  chapters: (Chapter & {
-    lessons: (Lesson & {
-      lessonAttendance: LessonAttendance;
-    })[];
-  })[];
+type Data = {
+  data: Attendance & {
+    course: Course & {
+      instructor: Instructor;
+      chapters: (Chapter & {
+        lessons: (Lesson & {
+          lessonAttendance: LessonAttendance;
+        })[];
+      })[];
+    };
+  };
 };
 
 type Args = {
@@ -24,17 +26,11 @@ type Args = {
 
 export const useFetchCourse = ({ attendanceId }: Args) => {
   const shouldFetch = attendanceId !== undefined;
-  const fetchUrl = shouldFetch ? `/api/v1/course?attendance_id=${attendanceId}` : null;
-  const {
-    data: course,
-    isLoading,
-    error,
-  } = useSWR<{
-    data: Data;
-  }>(fetchUrl, fetcher);
+  const fetchUrl = shouldFetch ? `/api/v1/attendance/${attendanceId}` : null;
+  const { data: attendance, isLoading, error } = useSWR<Data>(fetchUrl, fetcher);
 
   return {
-    course: course?.data,
+    attendance: attendance?.data,
     isLoading,
     error,
   };
