024bc0381e171ed100fd0bd3607a92492dbd342a
Kou <hikkappi@yahoo.co.jp>
2024-01-10 21:34:47 +0900

    タイトルが最大文字数を超えるときのエラーメッセージを表示。

--- features/lesson/schemas/PutSchema.ts
+++ features/lesson/schemas/PutSchema.ts
@@ -2,7 +2,10 @@ import * as yup from 'yup';
 import { PutLesson } from '../types/PutLesson';
 
 export const PutSchema: yup.Schema<PutLesson> = yup.object().shape({
-  title: yup.string().required('タイトルは必須です'),
+  title: yup
+    .string()
+    .max(50, 'タイトルは50文字以内です')
+    .required('タイトルは必須です'),
   remarks: yup.string().required('テキストは必須です'),
   url: yup.string().required('URLは必須です'),
   status: yup.string().required('状態は必須です'),
