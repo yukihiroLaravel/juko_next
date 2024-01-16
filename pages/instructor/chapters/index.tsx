08a1b2aca588d60f014dbed64bce50f303d7cc5a
Kou <hikkappi@yahoo.co.jp>
2024-01-16 20:36:51 +0900

    Fixed lesson card layout.

--- pages/instructor/chapters/index.tsx
+++ pages/instructor/chapters/index.tsx
@@ -126,8 +126,31 @@ const Index: NextPage = () => {
                       <div className="my-5 mx-auto w-11/12">
                         {chapter.lessons.map((lesson) => {
                           return (
-                            <div className="my-5" key={lesson.lesson_id}>
-                              {lesson.title}
+                            <div key={lesson.lesson_id} className="my-5">
+                              <div className="bg-[#ECF7FF] shadow-md rounded-md p-8 flex justify-between items-center">
+                                <div className="text-xl">{lesson.title}</div>
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  className="icon icon-tabler icon-tabler-dots-vertical text-gray-500"
+                                  width="24"
+                                  height="24"
+                                  viewBox="0 0 24 24"
+                                  strokeWidth="2"
+                                  stroke="currentColor"
+                                  fill="none"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                >
+                                  <path
+                                    stroke="none"
+                                    d="M0 0h24v24H0z"
+                                    fill="none"
+                                  />
+                                  <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                                  <path d="M12 19m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                                  <path d="M12 5m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+                                </svg>
+                              </div>
                             </div>
                           );
                         })}
