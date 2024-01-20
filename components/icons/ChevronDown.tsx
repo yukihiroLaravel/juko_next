63d8ce21df7a1b25dd0a099842353c76d1ccce2d
Kou <hikkappi@yahoo.co.jp>
2024-01-20 15:15:10 +0900

    Fixed property error.

--- components/icons/ChevronDown.tsx
+++ components/icons/ChevronDown.tsx
@@ -6,11 +6,11 @@ export const ChevronDown = () => {
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="currentColor"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M6 9l6 6l6 -6" />
