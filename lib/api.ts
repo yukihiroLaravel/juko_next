02d791c2c7462aace88c10da330bf3ced5c5fda2
Kou <hikkappi@yahoo.co.jp>
2024-04-03 22:14:42 +0900

    Fix: APIのヘッダーを更新

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,9 @@ export const Axios = axios.create({
   baseURL: 'http://localhost:8080',
   responseType: 'json',
   withCredentials: true,
+  withXSRFToken: true,
   headers: {
     'Content-Type': 'application/json',
+    Accept: 'application/json',
   },
 });
