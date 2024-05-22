b4fe1fc6d32fee7ec5684e1aebba04d291fca67b
mtakk <forsyuka321@gmail.com>
2024-05-22 21:43:45 +0900

    タイトル押下時にcoursesに遷移する

--- components/organisms/header/presentations/Header.tsx
+++ components/organisms/header/presentations/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React, { FC, ReactNode } from 'react';
 
 type Props = {
@@ -10,11 +11,11 @@ export const Header: FC<Props> = ({ isLogin = true, renderUserDropDown }) => {
     <nav className="sticky top-0 z-50 h-24 w-full bg-primary">
       <div className="flex min-h-full justify-between">
         <div className="flex items-center">
-          <a href="#" className="ml-5">
-            <h1 className="text-3xl font-bold text-[#FBF459]">
+          <Link href="/student/courses" className="ml-5">
+            <h1 className="cursor-pointer text-3xl font-bold text-[#FBF459]">
               受講管理アプリ
             </h1>
-          </a>
+          </Link>
         </div>
         {isLogin && renderUserDropDown()}
       </div>
