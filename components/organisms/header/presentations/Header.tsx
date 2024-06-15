bf9fdfec1b0d1e408cf2fc3f294c6ca4cdce434b
mtakk <forsyuka321@gmail.com>
2024-06-15 21:09:03 +0900

    マージン適用とリンクをnext仕様に修正

--- components/organisms/header/presentations/Header.tsx
+++ components/organisms/header/presentations/Header.tsx
@@ -7,15 +7,21 @@ type Props = {
   renderUserDropDown: () => ReactNode;
 };
 
-export const Header: FC<Props> = ({ isLogin = true, homeUrl, renderUserDropDown }) => {
+export const Header: FC<Props> = ({
+  isLogin = true,
+  homeUrl,
+  renderUserDropDown,
+}) => {
   return (
     <nav className="sticky top-0 z-50 h-24 w-full bg-primary">
       <div className="flex min-h-full justify-between">
         <div className="flex items-center">
-          <Link href={homeUrl} className="ml-5">
-            <h1 className="cursor-pointer text-3xl font-bold text-[#FBF459]">
-              受講管理アプリ
-            </h1>
+          <Link href={homeUrl} passHref>
+            <a className="ml-5">
+              <h1 className="cursor-pointer text-3xl font-bold text-[#FBF459]">
+                受講管理アプリ
+              </h1>
+            </a>
           </Link>
         </div>
         {isLogin && renderUserDropDown()}
