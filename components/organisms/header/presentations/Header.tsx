2eb6d546c6cf9f7856fd945e680441a189079196
mtakk <forsyuka321@gmail.com>
2024-06-15 14:03:52 +0900

    ヘッダーの遷移先を生徒側と講師側で変える

--- components/organisms/header/presentations/Header.tsx
+++ components/organisms/header/presentations/Header.tsx
@@ -3,15 +3,16 @@ import React, { FC, ReactNode } from 'react';
 
 type Props = {
   isLogin?: boolean;
+  homeUrl: string;
   renderUserDropDown: () => ReactNode;
 };
 
-export const Header: FC<Props> = ({ isLogin = true, renderUserDropDown }) => {
+export const Header: FC<Props> = ({ isLogin = true, homeUrl, renderUserDropDown }) => {
   return (
     <nav className="sticky top-0 z-50 h-24 w-full bg-primary">
       <div className="flex min-h-full justify-between">
         <div className="flex items-center">
-          <Link href="/student/courses" className="ml-5">
+          <Link href={homeUrl} className="ml-5">
             <h1 className="cursor-pointer text-3xl font-bold text-[#FBF459]">
               受講管理アプリ
             </h1>
