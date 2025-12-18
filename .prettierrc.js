3e046cdf9494def10e8594b6513fe6aed9fbb3da
Kou <hikkappi@yahoo.co.jp>
2025-12-18 21:09:38 +0900

    add: Prettier設定ファイルを追加し、package.jsonにフォーマットスクリプトを追加

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  useTabs: false,
+  tabWidth: 2,
+  singleQuote: true,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  semi: true,
+  embeddedLanguageFormatting: 'auto',
+};
