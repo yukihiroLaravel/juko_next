01b99f24f5ce18783c16faa2be0eb4e5d31ad0ef
satoko <kwutian253@gmail.com>
2026-01-22 07:48:38 +0900

    レビュー対応：デバッグ用ページを削除

--- src/app/debug/progress/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProgressSummary } from '@/features/attendance/components/ProgressSummary/ProgressSummary';
-
-export default function Page() {
-  return (
-    <div className="space-y-6 px-6 py-4">
-      <ProgressSummary />
-    </div>
-  );
-}
